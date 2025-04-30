f5bcdf4431515904eb02d400dd9d413190900391
Alice <alicetang2004@outlook.com>
2025-04-29 22:42:38 -0400

    syntax error

--- src/grid_mem.sv
+++ src/grid_mem.sv
@@ -17,7 +17,6 @@ module grid_mem #(parameter size = 1) (
     logic [`COLS-1:0] col_sel;
     logic [`ROWS-1:0][`COLS-1:0][size-1:0] mem_matrix;
     logic [`ROWS-1:0] clr_rows;
-    // logic [`COLS-1:0] clear_array;
     tri val;
 
     assign decoder_row = (row < 5'd20) ? row : '0;
@@ -81,63 +80,3 @@ module decoder
       end: decode_unit
     endgenerate
 endmodule: decoder
-
-// module grid_mem_test();
-//   logic [4:0] row;
-//   logic [3:0] col;
-//   logic w_val, r_val;
-//   logic re, we, shift, clear, valid;
-//   logic rst_n, clock;
-  
-//   grid_mem #(1) mem(.*);
-  
-//   initial begin
-//     rst_n = 1'b0;
-//     rst_n <= 1'b1;
-//     clock = 1'b0;
-//     forever #5 clock = ~clock;
-//   end
-  
-//   initial begin
-//     $monitor($time,, "re = %b, r_val = %b clear = %b valid = %b", re, r_val, clear, valid);
-//   end
-  
-//   initial begin
-//     $dumpfile("dump.vcd"); $dumpvars;
-//     row <= 5'd0;
-//     col <= 4'd0;
-//     w_val <= 1'd1;
-//     re <= 1'd0;
-//     we <= 1'd0;
-//     shift <= 1'd0;
-    
-//     @(posedge clock);
-//     row <= 5'd19;
-//     @(posedge clock);
-//     @(posedge clock);
-//     we <= '1;
-//     for (int i = 0; i < 12; i ++) begin
-//       col <= i;
-//       @(posedge clock);
-//     end
-//     we <= '0;
-//     @(posedge clock);
-//     re <= '1;
-//     for (int i = 0; i < 12; i ++) begin
-//       col <= i;
-//       @(posedge clock);
-//     end
-//     re <= '0;
-//     @(posedge clock);
-//     @(posedge clock);
-//     @(posedge clock);
-//     shift <= '1;
-//     @(posedge clock);
-//     shift <= '0;
-//     @(posedge clock);
-//     @(posedge clock);
-//     @(posedge clock);
-//     $dumpoff;
-//     $finish;
-//   end
-// endmodule: grid_mem_test
