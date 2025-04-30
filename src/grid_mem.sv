ccab4aa103e8c7de7ae0354ab1340b56b13759b0
Alice <alicetang2004@outlook.com>
2025-04-29 22:54:14 -0400

    remove size

--- src/grid_mem.sv
+++ src/grid_mem.sv
@@ -15,7 +15,7 @@ module grid_mem #(parameter size = 1) (
     logic [$clog2(`COLS)-1:0] decoder_col;
     logic [`ROWS-1:0] row_sel;
     logic [`COLS-1:0] col_sel;
-    logic [`ROWS-1:0][`COLS-1:0][size-1:0] mem_matrix;
+    logic [`ROWS-1:0][`COLS-1:0] mem_matrix;
     logic [`ROWS-1:0] clr_rows;
     tri val;
 
